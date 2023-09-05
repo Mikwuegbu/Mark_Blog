a15cb7d54ec8ca4e1ebfb9a03898b6f738658c3c
Mikwuegbu <mstephen130@yahoo.com>
2023-09-05 03:24:59 +0100

    Installed React and created some basic Styling

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import NavBar from './NavBar';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <div className="Content">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
