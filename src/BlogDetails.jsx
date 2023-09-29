6b92ba2c783d1ab9d49624048ba2c85efd248514
Mikwuegbu <mstephen130@yahoo.com>
2023-09-29 12:24:39 +0100

    added a delete button with an Onclick event

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,10 +1,20 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from './useFetch';
 //demostrating  route parameter (Dynamic routing)
 
 export const BlogDetails = () => {
     const { id } = useParams();
     const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/' + blogs.id, {
+            method: "DELETE",
+        }).then(() => {
+            history.push('/');
+        })
+
+    }
 
     return (
         <div className="blog-details">
@@ -14,7 +24,8 @@ export const BlogDetails = () => {
                 <article>
                     <h2>{blogs.title}</h2>
                     <p>Written by {blogs.author}</p>
-                    <div>{blogs.Body}</div>
+                    <div>{blogs.body}</div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
 
